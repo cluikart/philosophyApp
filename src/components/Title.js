c3502e29f5f8605dafba1c10199905551b41ab85
Shea Luikart <cluikart@trinity.edu>
2019-08-08 20:15:13 -0500

    Login Added

--- src/components/Title.js
+++ src/components/Title.js
@@ -23,11 +23,21 @@ const OpacityCard = pose.div({
     hidden: {opacity: 0}
 });
 
+const show = {
+    display: 'inline-block'
+  };
+  const hide= {
+    display: 'none'
+  };
+
 class Title extends React.Component {
     constructor(props) {
         super(props);
         this.setGo = this.setGo.bind(this);
         this.unsetGo = this.unsetGo.bind(this);
+        this.setPopup = this.setPopup.bind(this);
+        this.unsetPopup = this.unsetPopup.bind(this);
+        this.handleChildClick = this.handleChildClick.bind(this);
         this.state = {
             isGo: false,
         };
@@ -42,27 +52,68 @@ class Title extends React.Component {
         
     }
 
+    setPopup(person) {
+        this.setState({popup: true, currPers: person});
+        
+      }
+      unsetPopup() {
+        this.setState({popup: false});
+        
+      }
+      handleChildClick(e) {
+          e.stopPropagation();
+      }
+
 
     render() {
         return (
+            <div>
+                
+                <OpacityCard style={this.state.popup ? show : hide} 
+                        className="Login-popup-opacityCard" 
+                        pose={this.state.popup ? 'visible' : 'hidden'} 
+                        onClick={this.unsetPopup}
+                        >
+                  <div className="Login-popup" onClick={this.handleChildClick}>
+                    <div className="Login-popup-title-container">
+                      <h2 className="Login-popup-title">Login</h2>
+                      </div>
+                      <div className="Login-popup-text-container">
+                        <p className="Login-popup-text">Username or email</p>
+                        <input className="Login-input"></input>
+                        <p className="Login-popup-text">Password</p>
+                        <input className="Login-input"></input>
+                        <button className="Login-Btn">Sign In</button>
+                      </div>
+                  </div>
+                </OpacityCard> 
+                
             <div className="title-wrapper">
+
                 <Card className="card" id="card-1" pose={this.state.isGo ? 'visible' : 'hidden'}>
                     <img src={book} className="card-icon"/>
                 </Card>    
+
                 <Card className="card" id="card-2" pose={this.state.isGo ? 'visible' : 'hidden'}>
                     <img src={people} className="card-icon"/>
                 </Card>  
-                <Card className="card" id="card-3" pose={this.state.isGo ? 'visible' : 'hidden'}>
+
+                <Card className="card" id="card-3" 
+                pose={this.state.isGo ? 'visible' : 'hidden'}
+                onClick={this.setPopup}>
                     <img src={account} className="card-icon"/>
                 </Card>  
+
                 <OpacityCard pose={this.state.isGo ? 'hidden' : 'visible'}>
                 <p className="title-text">Explore A World of Ideas</p>
                 <button className="pageBtn" id="GOBtn" onClick={this.setGo}>GO</button>
                 </OpacityCard>
+
                 <div className="section title" onClick={this.unsetGo}>
                     <div className="title-image" ></div>
                 </div> 
-            </div>  
+            </div> 
+            </div> 
         )
     }
 }
--- src/components/carousel.js
+++ src/components/carousel.js
@@ -12,9 +12,23 @@ import lk from "../Images/Locke.jpg";
 import nz from "../Images/nietzsche.jpg";
 //import ari from "../Images/.jpg";
 
+let personMap = new Map();
+
+let descriptions = ["Aristotle (Greek: Ἀριστοτέλης Aristotélēs, pronounced [aristotélɛːs]; 384–322 BC)[A] was a Greek philosopher during the Classical period in Ancient Greece, the founder of the Lyceum and the Peripatetic school of philosophy and Aristotelian tradition. Along with his teacher Plato, he has been called the Father of Western Philosophy. His writings cover many subjects – including physics, biology, zoology, metaphysics, logic, ethics, aesthetics, poetry, theatre, music, rhetoric, psychology, linguistics, economics, politics and government. Aristotle provided a complex synthesis of the various philosophies existing prior to him, and it was above all from his teachings that the West inherited its intellectual lexicon, as well as problems and methods of inquiry. As a result, his philosophy has exerted a unique influence on almost every form of knowledge in the West and it continues to be a subject of contemporary philosophical discussion.",
+"The English philosopher Thomas Hobbes (1588-1679) is best known for his political thought, and deservedly so. His vision of the world is strikingly original and still relevant to contemporary politics. His main concern is the problem of social and political order: how human beings can live together in peace and avoid the danger and fear of civil conflict. He poses stark alternatives: we should give our obedience to an unaccountable sovereign (a person or group empowered to decide every social and political issue). Otherwise what awaits us is a state of nature that closely resembles civil war – a situation of universal insecurity, where all have reason to fear violent death and where rewarding human cooperation is all but impossible.",
+
+"Søren Aabye Kierkegaard (b. 1813, d. 1855) was a profound and prolific writer in the Danish “golden age” of intellectual and artistic activity. His work crosses the boundaries of philosophy, theology, psychology, literary criticism, devotional literature and fiction. Kierkegaard brought this potent mixture of discourses to bear as social critique and for the purpose of renewing Christian faith within Christendom. At the same time he made many original conceptual contributions to each of the disciplines he employed. He is known as the “father of existentialism”, but at least as important are his critiques of Hegel and of the German romantics, his contributions to the development of modernism, his stylistic experimentation, his vivid re-presentation of biblical figures to bring out their modern relevance, his invention of key concepts which have been explored and redeployed by thinkers ever since, his interventions in contemporary Danish church politics, and his fervent attempts to analyse and revitalise Christian faith.",
+
+"John Locke, (born August 29, 1632, Wrington, Somerset, England—died October 28, 1704, High Laver, Essex), English philosopher whose works lie at the foundation of modern philosophical empiricism and political liberalism. He was an inspirer of both the European Enlightenment and the Constitution of the United States. His philosophical thinking was close to that of the founders of modern science, especially Robert Boyle, Sir Isaac Newton, and other members of the Royal Society. His political thought was grounded in the notion of a social contract between citizens and in the importance of toleration, especially in matters of religion. Much of what he advocated in the realm of politics was accepted in England after the Glorious Revolution of 1688–89 and in the United States after the country’s declaration of independence in 1776.",
+
+"Friedrich Wilhelm Nietzsche (15 October 1844 – 25 August 1900) was a German philosopher, cultural critic, composer, poet, philologist, and Latin and Greek scholar whose work has exerted a profound influence on modern intellectual history. Nietzsche's body of work touched a wide range of topics, including art, philology, history, religion, tragedy, culture, and science. His writing spans philosophical polemics, poetry, cultural criticism, and fiction while displaying a fondness for aphorism and irony. His early inspiration was drawn from figures such as Arthur Schopenhauer, Richard Wagner, and Johann Wolfgang von Goethe. Prominent elements of his philosophy include his radical critique of truth in favor of perspectivism; his genealogical critique of religion and Christian morality and his related theory of master–slave morality; his aesthetic affirmation of existence in response to the 'death of God' and the profound crisis of nihilism;[14] his notion of the Apollonian and Dionysian; and his characterization of the human subject as the expression of competing wills, collectively understood as the will to power.[24] He also developed influential concepts such as the Übermensch and the doctrine of eternal return.[25][26] In his later work, he became increasingly preoccupied with the creative powers of the individual to overcome social, cultural and moral contexts in pursuit of new values and aesthetic health."];
+
+var imgUrls = ["Aristotle", "Hobbes", "Kierkegaard", "Locke", "Nietzsche"];
 
 
-var imgUrls = ["../Images/aristotle.jpg", "../Images/Hobbes.jpg", "../Images/kierkegaard.jpg", "../Images/Locke.jpg", "../Images/nietzsche.jpg"];
+for(let i = 0; i < imgUrls.length; i++){
+  personMap.set(imgUrls[i],descriptions[i]);
+};
 
 const OpacityCard = pose.div({
   visible: {opacity: 1},
@@ -33,28 +47,45 @@ class Carousel extends React.Component {
     constructor(props) {
         super(props);
         this.setPopup = this.setPopup.bind(this);
+        this.unsetPopup = this.unsetPopup.bind(this);
         this.state = {
             slideIndex: 0,
-            popup: false
+            popup: false,
+            currPers: ""
         };
     }
 
-    setPopup() {
-      this.setState({popup: true});
+    setPopup(person) {
+      this.setState({popup: true, currPers: person});
       console.log("popup is set");
+    }
+    unsetPopup() {
+      this.setState({popup: false});
+      console.log("popup is unset");
     }
 
     render() {
         
         return(
           <div>
-            <OpacityCard style={this.state.popup ? show : hide} className="carousel-popup-opacityCard" pose={this.state.popup ? 'visible' : 'hidden'}>
-                  <div className="carousel-popup"></div>
+            {/* The Carousel Popup */}
+            <OpacityCard style={this.state.popup ? show : hide} 
+                        className="carousel-popup-opacityCard" 
+                        pose={this.state.popup ? 'visible' : 'hidden'} 
+                        onClick={this.unsetPopup}>
+                  <div className="carousel-popup">
+                    <div className="carousel-popup-title-container">
+                      <h2 className="carousel-popup-title">{this.state.currPers}</h2>
+                      </div>
+                      <div className="carousel-popup-text-container">
+                        <p className="carousel-popup-text">{personMap.get(this.state.currPers)}</p>
+                      </div>
+                  </div>
                 </OpacityCard> 
             <div className="section carousel">
                 
                  
-            
+            {/* The Carousel */}
                 <Car 
                     slidesToShow={1}
                     cellAlign= "left"
@@ -81,19 +112,19 @@ class Carousel extends React.Component {
                       )}
                     >
 
-                    <div onClick={this.setPopup}>
+                    <div onClick={() => this.setPopup("Aristotle")}>
                     <img src={ari} alt="description of image" className="slide"/>
                     </div>
-                    <div onClick={this.setPopup}>
+                    <div onClick={() => this.setPopup("Locke")}>
                     <img src={lk} alt="description of image" className="slide"/>
                     </div>
-                    <div onClick={this.setPopup}>
+                    <div onClick={() => this.setPopup("Nietzsche")}>
                     <img src={nz} alt="description of image" className="slide"/>
                     </div>
-                    <div onClick={this.setPopup}>
+                    <div onClick={() =>this.setPopup("Kierkegaard")}>
                     <img src={kk} alt="description of image" className="slide"/>
                     </div >
-                    <div onClick={this.setPopup}>
+                    <div onClick={() => this.setPopup("Hobbes")}>
                     <img src={hb} alt="description of image" className="slide"/>
                     </div>
                 </Car>
@@ -135,20 +166,20 @@ class Carousel extends React.Component {
                     >
 
 
-                    <div className="slider-text-container">
-                        <h1 className="slider-text" id="aristotle">Aristotle</h1>
+                    <div className="slider-text-container" id="aristotle" >
+                        <h1 className="slider-text"  onClick={() => this.setPopup("Aristotle")}>Aristotle</h1>
                     </div>  
                     <div className="slider-text-container" id="locke">
-                        <h1 className="slider-text" >Locke</h1>
+                        <h1 className="slider-text" onClick={() => this.setPopup("Locke")}>Locke</h1>
                     </div>  
                     <div className="slider-text-container" id="nietzsche">
-                        <h1 className="slider-text" >Nietzsche</h1>
+                        <h1 className="slider-text"  onClick={() => this.setPopup("Nietzsche")}>Nietzsche</h1>
                     </div> 
                     <div className="slider-text-container" id="kierkegaard">
-                        <h1 className="slider-text" >kierkegaard</h1>
+                        <h1 className="slider-text" onClick={() => this.setPopup("Kierkegaard")}>Kierkegaard</h1>
                     </div> 
                     <div className="slider-text-container" id="hobbes">
-                        <h1 className="slider-text" >Hobbes</h1>
+                        <h1 className="slider-text" onClick={() => this.setPopup("Hobbes")}>Hobbes</h1>
                     </div> 
                 
                 </Car>  
